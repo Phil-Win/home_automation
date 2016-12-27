58b93696720e3e2a3041766f120fe5b3285f43ca
himynameisfil <nguye208@gmail.com>
2016-12-27 00:27:32 -0500

    Encryption tool and ARP script (#6)
    * Initial commit for the script that tracks MAC addresses
    
    * Made a project Folder
    
    * Completed RSA project along with ARP script (#5)
    
    * RSATool
    
    Completion of the message->Integer encoder
    
    * Key Generator for RSA
    
    Key Generator for RSA
    
    * Spacing issues
    
    Spacing issues
    
    * Spacing issues
    
    Spacing issues
    
    * Spacing issues
    
    * Spacing Issue
    
    * Spacing issues
    
    * Determining which MAC Addresses are connected to network
    
    also a rename
    
    * Complete Encryption Decryption Tool
    
    Currently will build manually and place jar manually... will look into
    gradle or ant later to automate it having it pulled from github and
    created

---
+++ Tools/RSATool/src/main/java/util/ASCIIMinus31Encoder.java
@@ -0,0 +1,51 @@
+package main.java.util;
+
+import java.math.BigInteger;
+
+public class ASCIIMinus31Encoder {
+	private String message;
+	private BigInteger messageAsBigIntASCIIMinus31;
+
+	public ASCIIMinus31Encoder(String inputMessage) {
+		this.message = inputMessage;
+		int charAsInt;
+		String newMessage = "";
+		for (char i: message.toCharArray()) {
+			charAsInt = ((byte) i) - 31;
+			if (charAsInt<0 && charAsInt>94) {
+				this.messageAsBigIntASCIIMinus31 = BigInteger.valueOf(0);
+				break;
+			}
+			newMessage += (charAsInt<10) ? "0"+String.valueOf(charAsInt) : String.valueOf(charAsInt);
+		}
+		this.messageAsBigIntASCIIMinus31 = new BigInteger(newMessage);
+	}
+
+	public ASCIIMinus31Encoder(BigInteger inputMessage) {
+		this.messageAsBigIntASCIIMinus31 = inputMessage;
+		String inputAsString = inputMessage.toString();
+		String newMessage = "";
+		
+		while(inputAsString.length() > 0) {
+			if (inputAsString.length()%2==1) {
+				newMessage+=(char)(Integer.parseInt((String) inputAsString.subSequence(0, 1))+31);
+				inputAsString = inputAsString.substring(1);
+			} else {
+				newMessage+=(char)(Integer.parseInt((String) inputAsString.subSequence(0, 2))+31);
+				inputAsString = inputAsString.substring(2);
+			}
+		}
+		
+		this.message = newMessage;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public BigInteger getMessageAsBigIntASCIIMinus31() {
+		return messageAsBigIntASCIIMinus31;
+	}
+	
+	
+}
